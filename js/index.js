393b7ce9a9813541ee44dcb888cdf758568ba0bf
JohnGrabowskiIII <johngrabowski97@gmail.com>
2021-02-11 17:20:14 -0500

    3 event listeners added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
-// Your code goes here
+// KEYDOWN ALERT
+let body = document.body;
+body.addEventListener('keydown', event => {
+    alert('You pressed the down key');
+});
+
+// MOUSEOVER H2 COLOR RED
+let headers = document.getElementsByTagName('h2');
+for (let i = 0; i < headers.length; i++) {
+    headers[i].addEventListener('mouseover', event => {
+        event.target.style.color = 'red';
+    });
+}
+
+// CLICK IMG RESIZE
+let allImgs = document.getElementsByTagName('img');
+console.log(allImgs);
+for (let i = 0; i < allImgs.length; i++) {
+    allImgs[i].addEventListener('click', event => {
+        let currentHeight = event.target.clientHeight;
+        let currentWidth = event.target.clientWidth
+        console.log(currentHeight, currentWidth);
+        event.target.style.height = ((currentHeight - 10) + "px");
+    })
+}
