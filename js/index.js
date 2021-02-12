32fdd2aa6c60e769e4b83a359f9a362e0206c4a6
JohnGrabowskiIII <johngrabowski97@gmail.com>
2021-02-11 20:22:40 -0500

    5 tasks done and stopPropogation added

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // KEYDOWN ALERT
 let body = document.body;
 body.addEventListener('keydown', event => {
-    alert('You pressed the down key');
+    alert('You pressed a key');
 });
 
 // MOUSEOVER H2 COLOR RED
@@ -14,12 +14,43 @@ for (let i = 0; i < headers.length; i++) {
 
 // CLICK IMG RESIZE
 let allImgs = document.getElementsByTagName('img');
-console.log(allImgs);
 for (let i = 0; i < allImgs.length; i++) {
     allImgs[i].addEventListener('click', event => {
         let currentHeight = event.target.clientHeight;
         let currentWidth = event.target.clientWidth
-        console.log(currentHeight, currentWidth);
         event.target.style.height = ((currentHeight - 10) + "px");
+        event.stopPropagation();
     })
 }
+
+// CHANGE NAV COLOR ON SCROLL
+function navHeadScrollEvent() {
+    window.addEventListener('scroll', event => {
+        document.querySelector('.main-navigation').style.backgroundColor = 'blue';
+    })
+}
+navHeadScrollEvent();
+
+// CONTENT GROW BORDER ON CLICK
+let content = document.querySelectorAll('.content-section');
+console.log(content);
+// PUT A FOREACH IN HERE
+content.forEach(contentSection => {
+    contentSection.addEventListener('click', event => {
+        switch (contentSection.style.border) {
+            case 'thin solid red':
+                contentSection.style.border = 'medium solid lime';
+                console.log('medium')
+                break;
+            case 'medium solid lime':
+                contentSection.style.border = 'thick solid black';
+                console.log('thick')
+                break;
+            default:
+                contentSection.style.border = 'thin solid red';
+                console.log(contentSection.style.border)
+                break;
+        }
+    })
+})
+    
